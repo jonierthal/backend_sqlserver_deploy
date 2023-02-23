ea7eb988b15cd5f2b50a55a22d8d7c27f2add96c
Jonathan Erthal <jonierthal@gmail.com>
2023-02-23 09:17:59 -0300

    Arrumado importação Cron function

--- src/cron.ts
+++ src/cron.ts
@@ -1,10 +1,12 @@
-const cron = require('cron');
+import { CronJob } from 'cron';
 import moment from 'moment-timezone';
 import { exec, ExecException } from 'child_process';
 
 // Configuração do cron job
 // Cria um objeto CronJob que dispara às 24:00 no horário local de Brasília todos os dias
-const job = new cron.CronJob('00 00 * * *', () => {
+export function Cron(){
+    
+    const job = new CronJob('00 00 * * *', () => {
     console.log(`Reiniciando o servidor às ${moment().tz('America/Sao_Paulo').format('HH:mm:ss')}`);
   
     // Insira aqui o código para reinicializar o servidor
@@ -17,7 +19,8 @@ const job = new cron.CronJob('00 00 * * *', () => {
     });
   }, null, true, 'America/Sao_Paulo');
   
-  console.log('CronJob iniciado');
+  console.log('CronJob iniciado!',job);
 // ...
+}
 
-export { cron };
+
